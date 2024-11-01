a77de02d9ae966d34c1bf37d54e687614f35c8f1
ThuanB1910150 <thuankg211@gmail.com>
2024-11-01 18:11:26 +0700

    Tao trang them moi mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
     },
+    {
+    path: "/contacts",
+    name: "contact.add",
+    component: () => import("@/views/Contactadd.vue"),
+    },  
  ]
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
