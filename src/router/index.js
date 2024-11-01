f6e662cfa3ffd3c5084f22daf7564c7ef2d8ecf6
ThuanB1910150 <thuankg211@gmail.com>
2024-11-01 17:34:44 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
  import { createWebHistory, createRouter } from "vue-router";
  import ContactBook from "@/views/ContactBook.vue";
  const routes = [
- {
- path: "/",
- name: "contactbook",
- component: ContactBook,
- },
- ];
- const router = createRouter({
- routes,
+    {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+    },
+    {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+    },
+ ]
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
  });
- history: createWebHistory(import.meta.env.BASE_URL),
+
  export default router;
